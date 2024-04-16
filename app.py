81db1e58c890b14bb736c4f9d3eb0ca8ef3d37a2
Breyden Taylor <124335295+prompted365@users.noreply.github.com>
2024-04-15 21:53:31 -0400

    port assignment

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def df_to_xml(df):
     return ET.tostring(root, encoding='unicode', method='xml')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
